5098198493c401dcb6590d23fd6320e277cd5434
freemso <freemso@hotmail.com>
2017-09-15 17:51:31 +0800

    add color for finished and unfinished task marks

--- view.py
+++ view.py
@@ -5,6 +5,7 @@
     To view the distribution of data over geographic plane
 """
 import folium
+from folium import Icon
 
 import dataset
 
@@ -15,20 +16,48 @@ NEW_TASKS_MAP_FILENAME = "map_view/new_tasks.html"
 OLD_TASKS_MAP_FILENAME = "map_view/old_tasks.html"
 
 
-def map_it(objects, filename):
-    assert all("latitude" in dir(o) and "longitude" in dir(o) for o in objects)
-    lat_lon = [
-        (o.latitude, o.longitude) for o in objects
-    ]
-    geo_map = folium.Map()
-    for coord in lat_lon:
-        folium.Marker(location=[coord[0], coord[1]]).add_to(geo_map)
-    geo_map.save(filename)
-
-
 if __name__ == '__main__':
     users, new_tasks, old_tasks = dataset.load_data()
-    map_it(users, USER_MAP_FILENAME)
-    map_it(new_tasks, NEW_TASKS_MAP_FILENAME)
-    map_it(old_tasks, OLD_TASKS_MAP_FILENAME)
+    max_lon = max([t.longitude for t in old_tasks])
+    min_lon = min([t.longitude for t in old_tasks])
+    max_lat = max([t.latitude for t in old_tasks])
+    min_lat = min([t.latitude for t in old_tasks])
+
+    geo_map = folium.Map(
+        location=[23, 113.5],
+        zoom_start=9
+    )
+    for t in old_tasks:
+        icon = Icon(
+            color="green" if t.is_finished else "red",
+            prefix="fa",
+            icon="circle"
+        )
+        folium.Marker(
+            location=[t.latitude, t.longitude],
+            icon=icon
+        ).add_to(geo_map)
+    # for t in new_tasks:
+    #     icon = Icon(
+    #         icon_color="gray",
+    #         prefix="fa",
+    #         icon="circle"
+    #     )
+    #     folium.Marker(
+    #         location=[t.latitude, t.longitude],
+    #         icon=icon
+    #     ).add_to(geo_map)
+    # for u in users:
+    #     if min_lat < u.latitude < max_lat and min_lon < u.longitude < max_lon:
+    #         icon = Icon(
+    #             color="blue",
+    #             prefix="fa",
+    #             icon="user"
+    #         )
+    #         folium.Marker(
+    #             location=[u.latitude, u.longitude],
+    #             icon=icon
+    #         ).add_to(geo_map)
+
+    geo_map.save(OLD_TASKS_MAP_FILENAME)
 
